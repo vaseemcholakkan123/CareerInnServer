9243d7ba99f7c7608dcdc2aa6a7c323b60b0740d
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-08-11 16:52:38 +0530

    djange.setup for registry error

--- CareerInn/asgi.py
+++ CareerInn/asgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
 from .middlewares import JwtAuthForAsgiStack
 from channels.auth import AuthMiddlewareStack
 from CareerInnsocket.routing import websocket_urlpatterns
