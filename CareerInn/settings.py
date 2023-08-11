a018f69cf5692820fb3924137e0b14291076ded0
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-08-11 17:18:16 +0530

    updated media urls

--- CareerInn/settings.py
+++ CareerInn/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "User.User"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/careerinn-api/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
