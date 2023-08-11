b5edf333dd6af13ca8ffc2d1f34d7abb171c5b2c
Vaseem Cholakkan <vaseemcholakkan@gmail.com>
2023-08-11 17:25:24 +0530

    routing url change

--- CareerInnsocket/routing.py
+++ CareerInnsocket/routing.py
@@ -5,9 +5,9 @@ from django.urls import path
 from .consumers import PeerConnectionConsumer,NotificationConsumer,ChatConsumer
 
 websocket_urlpatterns = [
-    path('peersocket/',PeerConnectionConsumer.as_asgi()),
-    path('notifications/',NotificationConsumer.as_asgi()),
-    path('chat/',ChatConsumer.as_asgi()),
+    path('careerinn-api/peersocket/',PeerConnectionConsumer.as_asgi()),
+    path('careerinn-api/notifications/',NotificationConsumer.as_asgi()),
+    path('careerinn-api/chat/',ChatConsumer.as_asgi()),
 ]
 
 
